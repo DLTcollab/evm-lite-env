4f7f2dad8f78976ddc54b0097b61f26503c8bbf5
mushroom0117 <v860117@gmail.com>
2019-01-08 00:49:59 +0800

    Update the example code Make variable and comment more clearly and easier to understand. Use the `DataDirectory` class instead of keystore and access the keystore like `DataDirectory.keystore`, let the relationship between keysotre and data directory clearer.

--- example/deploySmartContractLocally.js
+++ example/deploySmartContractLocally.js
@@ -4,19 +4,24 @@ const Accounts = require('evm-lite-lib').Account;
 const solc = require('solc');
 const fs = require('fs');
 
-// Transaction Addresses
+// Default from address
 const from = '0x1a5c6b111e883d920fd24fee0bafae838958fa05';
 
-// evm-lite object
+// Config evm-lite IP
 const nodeIP = '127.0.0.1';
 
-// Keystore 
-const KeystorePath = '/Users/junwei/.evmlc';
-const password =  'superpassword';
+// Data directory object
+const dataDirectory = new evmlib.DataDirectory('/Users/junwei.evmlc');
 
-// Contract Object
+// Password for keystore
+const password = 'superpassword';
+
+// Contract object
 const contractPath = '/Users/junwei/Documents/CrowdFunding.sol';
 const rawContractname = 'CrowdFunding';
+
+const contractFile = fs.readFileSync(contractPath, 'utf8');
+const contractName = ':' + rawContractname;
 
 // EVMLC object
 const evmlc = new EVMLC(nodeIP , 8080, {
@@ -25,27 +30,32 @@ const evmlc = new EVMLC(nodeIP , 8080, {
     gasPrice: 0
 });
 
-const keystore = new KeyStore(KeystorePath, 'keystore');
-
-const contractFile = fs.readFileSync(contractPath, 'utf8');
-const contractName = ':' + rawContractname;
-
 const output = solc.compile(contractFile, 1);
 const ABI = JSON.parse(output.contracts[contractName].interface);
 
 // Deploy The Smart Contract
 async function deploySmartContract() {
 
-    // Decrypted The Account
-    const keystoreFile = await keystore.get(from);
-    const decryptedAccount = Accounts.decrypt(keystoreFile, password);
-    const DeployedContract = evmlc.generateContractFromABI(ABI);
+    // Get keystore object from the keystore directory
+    // For the from address so we can decrypt and sign
+    const keystoreFile = await dataDirectory.keystore.get(from); 
 
+    // Decrypt the account
+    const decryptedAccount = Accounts.decrypt(keystoreFile, password);
+
+    // Bytecode of compiled account
+    const DeployedContract = await evmlc.generateContractFromABI(ABI);
+
+    // Generate contract to deploy later
     DeployedContract.data(output.contracts[contractName].bytecode);
 
+    // Generate deployment transaction
     const deployTransaction = DeployedContract.deploy({parameters: [10000]});
 
+    // Sign transaction with decrypted account
     const signedTransaction = await decryptedAccount.signTransaction(deployTransaction);
+
+    // Send deployment transaction
     return await deployTransaction.sendRaw(signedTransaction.rawTransaction);
 }
 
--- example/transferKeystoreLocally.js
+++ example/transferKeystoreLocally.js
@@ -7,28 +7,37 @@ const to = '0xeF5D1019e5B752E993BA17EC3aD2B021872Fb969';
 // Transaction Value
 const value = 200;
 
-// evm-lite object
+// Config evm-lite IP
 const nodeIP = '127.0.0.1';
 
-// Keystore 
-const KeystorePath = '/Users/junwei/.evmlc';
-const password =  'superpassword';
+// Data directory object
+const dataDirectory = new evmlib.DataDirectory('/Users/junwei.evmlc');
+
+// Password for keystore
+const password = 'superpassword';
 
 // EVMLC object
 const evmlc = new evmlib.EVMLC(nodeIP, 8080, {
+    from,
     gas: 100000,
     gasPrice: 0
 });
 
-const keystore = new evmlib.Keystore(KeystorePath, 'keystore');
-
 async function signTransactionLocally() {
 
-    // Decrypted The Account
-    const keystoreFile = await keystore.get(from);
-    const decryptedAccount = evmlib.Account.decrypt(keystoreFile,password);
-    const transaction = await evmlc.prepareTransfer(to, value, from);
+    // Get keystore object from the keystore directory
+    // For the from address so we can decrypt and sign
+    const keystoreFile = await dataDirectory.keystore.get(from); 
+
+    // Decrypt the v3JSONKeystore file so expose `sign` function
+    const decryptedAccount = evmlib.Account.decrypt(keystoreFile, password);
+
+    // Prepare a transaction
+    const transaction = await evmlc.prepareTransfer(to, value);
+
+    // Sign transaction and return a new Transaction object
     const signedTransaction = await decryptedAccount.signTransaction(transaction);
+
     return await transaction.sendRaw(signedTransaction.rawTransaction);
 }
 
