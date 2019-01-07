11a5c35ce3a5bed989b6149ed5e437c8e278b6d1
mushroom0117 <v860117@gmail.com>
2019-01-07 15:06:07 +0800

    Update the example code

--- example/deploySmartContractLocally.js
+++ example/deploySmartContractLocally.js
@@ -4,31 +4,41 @@ const Accounts = require('evm-lite-lib').Account;
 const solc = require('solc');
 const fs = require('fs');
 
-const from = '0x1a5c6b111e883d920fd24fee0bafae838958fa05'
+// Transaction Addresses
+const from = '0x1a5c6b111e883d920fd24fee0bafae838958fa05';
+
+// evm-lite object
+const nodeIP = '127.0.0.1';
+
+// Keystore 
+const KeystorePath = '/Users/junwei/.evmlc';
+const password =  'superpassword';
+
+// Contract Object
+const contractPath = '/Users/junwei/Documents/CrowdFunding.sol';
+const rawContractname = 'CrowdFunding';
 
 // EVMLC object
-const evmlc = new EVMLC('127.0.0.1', 8080, {
+const evmlc = new EVMLC(nodeIP , 8080, {
     from,
     gas: 1000000,
     gasPrice: 0
 });
 
-// Keystore object
-const keystore = new KeyStore('/Users/junwei/.evmlc', 'keystore');
+const keystore = new evmlib.Keystore(KeystorePath, 'keystore');
 
-// Contract Object
-const contractPath = '/Users/junwei/Documents/CrowdFunding.sol';
 const contractFile = fs.readFileSync(contractPath, 'utf8');
-const contractName = ':' + 'CrowdFunding';
+const contractName = ':' + rawContractname;
 
 const output = solc.compile(contractFile, 1);
 const ABI = JSON.parse(output.contracts[contractName].interface);
 
 // Deploy The Smart Contract
 async function deploySmartContract() {
+
     // Decrypted The Account
     const keystoreFile = await keystore.get(from);
-    const decryptedAccount = Accounts.decrypt(keystoreFile, 'superpassword');
+    const decryptedAccount = Accounts.decrypt(keystoreFile, password);
     const DeployedContract = evmlc.generateContractFromABI(ABI);
 
     DeployedContract.data(output.contracts[contractName].bytecode);
--- example/transferKeystoreLocally.js
+++ example/transferKeystoreLocally.js
@@ -4,19 +4,30 @@ const evmlib = require('evm-lite-lib');
 const from = '0xeb0d3811B16f792c119E6395141fa58f8409C597';
 const to = '0xeF5D1019e5B752E993BA17EC3aD2B021872Fb969';
 
+// Transaction Value
+const value = 200;
+
+// evm-lite object
+const nodeIP = '127.0.0.1';
+
+// Keystore 
+const KeystorePath = '/Users/junwei/.evmlc';
+const password =  'superpassword';
+
 // EVMLC object
-const evmlc = new evmlib.EVMLC('127.0.0.1', 8080, {
+const evmlc = new evmlib.EVMLC(nodeIP, 8080, {
     gas: 100000,
     gasPrice: 0
 });
 
-// Keystore object
-const keystore = new evmlib.Keystore('/Users/junwei/.evmlc', 'keystore');
+const keystore = new evmlib.Keystore(KeystorePath, 'keystore');
 
 async function signTransactionLocally() {
+
+    // Decrypted The Account
     const keystoreFile = await keystore.get(from);
-    const decryptedAccount = evmlib.Account.decrypt(keystoreFile, 'superpassword');
-    const transaction = await evmlc.prepareTransfer(to, 200, from);
+    const decryptedAccount = evmlib.Account.decrypt(keystoreFile,password);
+    const transaction = await evmlc.prepareTransfer(to, value, from);
     const signedTransaction = await decryptedAccount.signTransaction(transaction);
     return await transaction.sendRaw(signedTransaction.rawTransaction);
 }
