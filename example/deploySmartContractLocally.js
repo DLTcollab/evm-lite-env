7a1e8b2b01895cd5f04dd94dd9c74042814a213e
Junwei <v860117@gmail.com>
2019-01-17 00:42:13 +0800

    Enforce consistent coding style (#9)
    This project defers to Airbnb JavaScript Style Guide.

--- example/deploySmartContractLocally.js
+++ example/deploySmartContractLocally.js
@@ -5,44 +5,45 @@ const solc = require('solc');
 const fs = require('fs');
 
 // Default from address
-const from = '[from address]';
+const from = '';
 
 // EVMLC object
 const evmlc = new EVMLC('127.0.0.1', 8080, {
-    from,
-    gas: 1000000,
-    gasPrice: 0
+  from,
+  gas: 1000000,
+  gasPrice: 0,
 });
 
 // Keystore object
-const dataDirectory = new DataDirectory('[evmlc path]/.evmlc');
-const password = '[password]';
+const dataDirectory = new DataDirectory('');
+const password = '';
 
 
 // Contract Object
-const contractPath = '[contract path]';
+const contractPath = '';
+const rawContractName = '';
+
 const contractFile = fs.readFileSync(contractPath, 'utf8');
-const contractName = ':' + '[contract name]';
+const contractName = ':' + rawContractName;
 
 const output = solc.compile(contractFile, 1);
 const ABI = JSON.parse(output.contracts[contractName].interface);
 const data = output.contracts[contractName].bytecode;
 
 const generateContract = async () => {
+  // Get keystore object from the keystore directory
+  // For the from address so we can decrypt and sign
+  const account = await dataDirectory.keystore.decrypt(from, password);
 
-    // Get keystore object from the keystore directory
-    // For the from address so we can decrypt and sign
-    const account = await dataDirectory.keystore.decrypt(from, password);
+  // Generate contract object with ABI and data
+  const contract = await evmlc.generateContractFromABI(ABI, data);
 
-    // Generate contract object with ABI and data
-    const contract = await evmlc.generateContractFromABI(ABI, data);
+  // Deploy and return contract with functions populated
+  const response = await contract.deploy(account, { parameters: [] });
 
-    // Deploy and return contract with functions populated
-    return await contract.deploy(account, {
-        parameters: [100000]
-    });
+  return response;
 };
 
 generateContract()
-    .then((contract) => console.log(contract.options.address))
-    .catch((error) => console.log(error));
+  .then(contract => console.log(contract.options.address))
+  .catch(error => console.log(error));
